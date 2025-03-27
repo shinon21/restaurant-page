270634248592ac6116a159d1b8ed568c78653aa1
Shinon <204764867+shinon21@users.noreply.github.com>
2025-03-27 13:36:35 -0500

    Change output name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
         clean: true
     },
