0ef7e6fa56ceed8d4edbec2ab6b6ac6e30efd288
Shinon <204764867+shinon21@users.noreply.github.com>
2025-03-27 10:40:30 -0500

    Handle js image importing

--- src/home.js
+++ src/home.js
@@ -1,13 +1,14 @@
 import { createElement } from "./dom"
+import asuna from "./assets/asuna.jpg";
 
 const createHome = () => {
     const main = createElement("main", "", { class: "home" });
     const heading = createElement("h2", "Home");
     const introduction = createElement("p",
         "Welcome to the Sword Art Online restaurant. Here you will find some dishes from the anime. I hope you like them.",
-        { class: "introduction" })
-
-    main.append(heading, introduction);
+        { class: "introduction" });
+    const asunaImage = createElement("img", "", { src: asuna });
+    main.append(heading, introduction, asunaImage);
     return main;
 }
 
