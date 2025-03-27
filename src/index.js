4ec02e63a66d6627503efb7e1ce279a393212b23
Shinon <204764867+shinon21@users.noreply.github.com>
2025-03-27 11:30:26 -0500

    Handle button navigation

--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ const content = document.querySelector("#content");
 const home = createHome();
 const menu = createMenu();
 
+const paths = { "home": home, "menu": menu, }
+
+const handleNavigation = () => {
+    const navButtons = document.querySelectorAll("nav button");
+    for (const button of navButtons) {
+        button.addEventListener("click", (e) => {
+            const path = e.target.textContent.toLowerCase();
+            switchContent(paths[path]);
+        })
+    }
+}
+
 const switchContent = (newContent) => {
     content.replaceChildren();
     content.append(newContent);
 }
 
-switchContent(menu);
+switchContent(home);
+handleNavigation();
